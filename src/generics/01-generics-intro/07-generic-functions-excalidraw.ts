999e4203f805323299bc760da36a9e391ee67214
wangkaiwd <1162486176@qq.com>
2023-01-31 11:57:59 +0800

    feat: complete generic intro

---
+++ src/generics/01-generics-intro/07-generic-functions-excalidraw.ts
@@ -0,0 +1,9 @@
+const sum = <T> (array: readonly T[], mapper: (item: T) => number): number =>
+  array.reduce((acc, item) => acc + mapper(item), 0);
+const arr = ['1', '2', '5'];
+
+
+const result = sum(arr, (item) => {
+  return parseInt(item);
+})
+
