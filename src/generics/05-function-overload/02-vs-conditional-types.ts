c5658d845a7e956e4162e06b2f14df86473e2da2
wangkaiwd <1162486176@qq.com>
2023-03-01 17:59:13 +0800

    feat: type safer function overload

--- src/generics/05-function-overload/02-vs-conditional-types.ts
+++ src/generics/05-function-overload/02-vs-conditional-types.ts
@@ -1,12 +1,19 @@
-// overload signature
+// // overload signature
 function youSayGoodbyeISayHello (greeting: 'hello'): 'goodbye'
 function youSayGoodbyeISayHello (greeting: 'goodbye'): 'hello'
+//
+// // implement signature
+// // the signature of implementation is not visible from outside.
+// // the implementation signature must also be compatible with overload signature
+// function youSayGoodbyeISayHello (greeting: 'hello' | 'goodbye') {
+//   return greeting === 'goodbye' ? 'hello' : 'goodbye';
+// }
 
-// implement signature
-// the signature of implementation is not visible from outside.
-// the implementation signature must also be compatible with overload signature
-function youSayGoodbyeISayHello (greeting: 'hello' | 'goodbye') {
+// add return types to original function
+// make function overloads more type safe
+function youSayGoodbyeISayHello (greeting: 'hello' | 'goodbye'): 'hello' | 'goodbye' {
   return greeting === 'goodbye' ? 'hello' : 'goodbye';
 }
 
 const x = youSayGoodbyeISayHello('hello');
+
